c354e83b3dd079c60bce2fd400460ca2443a9cd1
klmkyo <mail@mklimek.dev>
2023-01-17 22:19:58 +0100

    Update ui (does not build)

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,16 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/screen/string.hpp>
+#include <ftxui/component/component_options.hpp>  // for InputOption
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/component/component.hpp>
 #include <Database/database.hpp>
 #include <Repositories/repos.hpp>
 #include <Repositories/user.hpp>
-#include <ftxui/component/component_options.hpp>  // for InputOption
+
+using namespace ftxui;
+
+Component ModalComponent(std::function<void()> do_nothing, std::function<void()> hide_modal);
 
 int main() {
 
@@ -36,7 +40,6 @@ int main() {
     // }
     // --------------
 
-    using namespace ftxui;
     std::string login;
     std::string password;
 
@@ -45,15 +48,47 @@ int main() {
     password_option.password = true;
     Component input_password = Input(&password, "hasło", password_option);
 
-    Component button = Button("Zaloguj się", [&] {
-        std::cout << "Login: " << login << std::endl;
-        std::cout << "Password: " << password << std::endl;
+    bool show_modal = false;
+
+    Component registerbutton = Button("Zarejestruj się", [&] {
+        auto status = Register(login, password);
+        switch (status)
+        {
+            case RegisterStatus::SUCCESS:
+            std::cout << "Zarejestrowano" << std::endl;
+            break;
+            case RegisterStatus::USER_EXISTS:
+            std::cout << "Użytkownik już istnieje" << std::endl;
+            break;
+            case RegisterStatus::INTERNAL_ERROR:
+            std::cout << "Błąd wewnętrzny" << std::endl;
+            break;
+        }
+    });
+
+    Component loginbutton = Button("Zaloguj się", [&] {
+        auto result = Login(login, password);
+        switch (result.status)
+        {
+            case LoginStatus::SUCCESS:
+            std::cout << "Zalogowano" << std::endl;
+            break;
+            case LoginStatus::WRONG_PASSWORD:
+            std::cout << "Błędne hasło" << std::endl;
+            break;
+            case LoginStatus::USER_NOT_FOUND:
+            std::cout << "Użytkownik nie istnieje" << std::endl;
+            break;
+            case LoginStatus::INTERNAL_ERROR:
+            std::cout << "Błąd wewnętrzny" << std::endl;
+            break;
+        }
     });
 
     auto component = Container::Vertical({
         input_first_name,
         input_password,
-        button,
+        loginbutton,
     });
 
     auto renderer = Renderer(component, [&] {
@@ -65,13 +100,39 @@ int main() {
                         input_password->Render() | borderLight
                     ),
                     separator(),
-                    button->Render()
+                    loginbutton->Render()
                     )
                 ) | size(WIDTH, EQUAL, 60)
             );
     });
 
+    auto hide_modal = [&] { show_modal = false; };
+    auto do_nothing = [&] {};
+    auto modal_component = ModalComponent(do_nothing, hide_modal);
+
+    renderer |= Modal();
+
     auto screen = ScreenInteractive::Fullscreen();
     screen.Loop(renderer);
     return 0;
+}
+
+// Definition of the modal component. The details are not important.
+Component ModalComponent(std::function<void()> do_nothing,
+                         std::function<void()> hide_modal) {
+  auto component = Container::Vertical({
+      Button("Do nothing", do_nothing),
+      Button("Quit modal", hide_modal),
+  });
+  // Polish how the two buttons are rendered:
+  component |= Renderer([&](Element inner) {
+    return vbox({
+               text("Modal component "),
+               separator(),
+               inner,
+           })
+           | size(WIDTH, GREATER_THAN, 30)
+           | border;
+  });
+  return component;
 }
