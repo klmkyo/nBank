00b48a62c1162fa9485adc5fb3d1d8cb8c71293e
famouzkk <famouzkk@gmail.com>
2023-01-23 21:23:50 +0100

    added TransactionData constructor, added transaction logging

--- src/Database/structures.hpp
+++ src/Database/structures.hpp
@@ -6,13 +6,13 @@
 
 struct User {
   int id;
+
   std::string name;
   std::string login;
   std::string password_hash;
   std::string password_salt;
 
-  friend std::ostream& operator<<(std::ostream& os, const User& u)
-  {
+  friend std::ostream& operator<<(std::ostream& os, const User& u) {
     return os << "User(id=" << u.id << ", name=" << u.name << " login=" << u.login << " password_hash=" << u.password_hash << " password_salt=" << u.password_salt << ")";
   }
 };
@@ -27,8 +27,7 @@ struct Account {
   double balance;
   int phone_number;
 
-  friend std::ostream& operator<<(std::ostream& os, const Account& acc)
-  {
+  friend std::ostream& operator<<(std::ostream& os, const Account& acc) {
     return os << "Account(id=" << acc.id << ", name=" << acc.name << ", user="<<acc.user_id<<", balance="<<acc.balance<<", phone_number="<<acc.phone_number<<")";
   }
 };
@@ -45,8 +44,7 @@ struct CreditCard {
   int expiration_year;
   int pin;
 
-  friend std::ostream& operator<<(std::ostream& os, const CreditCard& cc)
-  {
+  friend std::ostream& operator<<(std::ostream& os, const CreditCard& cc) {
     return os << "CreditCard(id=" << cc.id << ", name=" << cc.name << ", account_id="<<cc.account_id
     <<", number="<<cc.number<<", cvv="<<cc.cvv<<", expiration_month="<<cc.expiration_month<<", expiration_year="
     <<cc.expiration_year<<", pin="<<cc.pin<<")";
@@ -54,13 +52,20 @@ struct CreditCard {
 };
 
 
-struct TransactionH {
+struct TransactionData {
   uint32_t id;
 
   uint32_t sender_id;
   uint32_t recipent_id;
-  std::string title;
   double amount;
+
+  TransactionData(uint32_t sender_id, uint32_t recipent_id, double amount): 
+                  sender_id(sender_id), recipent_id(recipent_id), amount(amount) {}
+  TransactionData(const Account& sender, const Account& recipent, double amount) : amount(amount) {
+    sender_id = sender.user_id;
+    recipent_id = recipent.user_id;
+  } 
+                  
 };
 
 /* Structure Builder - should be used only once (in Database definition)*/
@@ -71,6 +76,7 @@ class StructureBuilder{
     'users' table -> User
     'accounts' table -> Account
     'credit_cards' table -> CreditCard
+    'transactions' table -> TransactionData
   */
   static auto _build_structure(const char* path = "")
     {
@@ -97,12 +103,11 @@ class StructureBuilder{
                         make_column("expiration_month", &CreditCard::expiration_month),
                         make_column("expiration_year", &CreditCard::expiration_year),
                         make_column("pin", &CreditCard::pin)),
-                make_table<TransactionH>("transactions",
-                        make_column("id", &TransactionH::id, autoincrement(), primary_key()),
-                        make_column("sender_id", &TransactionH::sender_id),
-                        make_column("recipent_id", &TransactionH::recipent_id),
-                        make_column("title", &TransactionH::title),
-                        make_column("amount", &TransactionH::amount)));
+                make_table<TransactionData>("transactions",
+                        make_column("id", &TransactionData::id, autoincrement(), primary_key()),
+                        make_column("sender_id", &TransactionData::sender_id),
+                        make_column("recipent_id", &TransactionData::recipent_id),
+                        make_column("amount", &TransactionData::amount)));
     };
 };
 
