41874fc4d8fd1b6f2635bba0c597e0a34e87ef20
klmkyo <mail@mklimek.dev>
2023-01-24 21:29:09 +0100

    Fix outdated header file

--- src/UI/transferui.hpp
+++ src/UI/transferui.hpp
@@ -4,6 +4,6 @@
 // they need to be void, they was set to ftxui::Component which is typename for shared_ptr, even that in transferui.cpp they was set to void something overrided them 
 void HandleTransactionResult(const TransactionResult& result);
 void DirectTransferPanel(Account& account);
-void BLIKTransferPanel(Account& account, std::function<void()> exit_func);
+void BLIKTransferPanel(Account& account);
 void DepositPanel();
 void TransferPanel(Account& account);
