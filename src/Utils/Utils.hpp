f721839707a1aedbaa4243acd2ed056c459e57d2
klmkyo <mail@mklimek.dev>
2023-01-24 20:43:58 +0100

    Update header file

--- src/Utils/Utils.hpp
+++ src/Utils/Utils.hpp
@@ -11,7 +11,7 @@ namespace Utils {
     
     Type::HashedPassword HashPassword(const std::string& password);
     std::string gen_random(const int len);
-    std::string gen_random_num(const int len);
+    std::string gen_random_num(const uint len);
     bool verify_password(const std::string& password, const std::string& hash, const std::string& salt);
     std::string simple_sha256_hash(const std::string& input);
     std::string double_to_string(double d);
